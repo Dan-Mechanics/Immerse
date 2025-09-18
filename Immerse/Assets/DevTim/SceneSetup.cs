ba999e46a6031a452b436cd1fb261cc745daa2b1
Dan-Mechanics <github.tim@desmidt.org>
2025-09-18 22:52:24 +0200

    scenesetup.cs

---
+++ Immerse/Assets/DevTim/SceneSetup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Spellfight
+{
+    public class SceneSetup : MonoBehaviour
+    {
+        [SerializeField] [Min(1)] private int fps = default;
+        [SerializeField] private float fixedUpdateRate = default;
+        [SerializeField] private bool locked = default;
+
+        private void Start()
+        {
+            Application.targetFrameRate = fps;
+            Time.fixedDeltaTime = 1f / fixedUpdateRate;
+            Cursor.visible = !locked;
+            Cursor.lockState = locked ? CursorLockMode.Locked : CursorLockMode.None;
+        }
+    }
+}
