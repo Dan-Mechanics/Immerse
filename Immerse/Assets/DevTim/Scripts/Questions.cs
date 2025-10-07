fc68488dd9183075a22cbb70c8dfe53e9973dcb2
Dan-Mechanics <github.tim@desmidt.org>
2025-10-07 13:22:31 +0200

    Revert "working on questions system"
    This reverts commit fe6d58c5e6f05578abcc92964ac1cef0328eff68.

--- Immerse/Assets/DevTim/Scripts/Questions.cs
+++ Immerse/Assets/DevTim/Scripts/Questions.cs
@@ -1,38 +1,17 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace Immerse
 {
     [Serializable]
-    public struct Questions
+    public class Questions
     {
-        public int Length => options.Length;
-        
-        public int processingAppliesTo;
-        public Color baseColor;
-        public float saturation;
-        public Color colorA;
-        public Color colorB;
-        public Actor actor;
         public List<Replace> replaceCalls;
+        
         public Prompter.Option[] options;
-
-        public void Process() 
-        {
-            if (processingAppliesTo > options.Length)
-                processingAppliesTo = options.Length;
-            
-            for (int i = 0; i < processingAppliesTo; i++)
-            {
-                options[i].color = Color.Lerp(baseColor, Color.Lerp(colorA, colorB, (float)i / options.Length), saturation);
-                options[i].icon = actor.icon;
-                for (int j = 0; j < replaceCalls.Count; j++)
-                {
-                    options[i].text = options[i].text.Replace(replaceCalls[j].target, replaceCalls[j].with);
-                }
-            }
-        }
+        
         
         [Serializable]
         public struct Replace
