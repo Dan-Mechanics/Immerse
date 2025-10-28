b0818e2402a8a0b525d64627c12ecf42c17e2a37
Dan-Mechanics <github.tim@desmidt.org>
2025-10-28 16:08:50 +0100

    voice acting

--- Immerse/Assets/DevTim/Scripts/Composites/TextWriter.cs
+++ Immerse/Assets/DevTim/Scripts/Composites/TextWriter.cs
@@ -8,7 +8,7 @@ namespace Immerse
     [RequireComponent(typeof(TMP_Text))]
     public class TextWriter : StateElement
     {
-        public const float INTERVAL = 0.0625f;
+        public const float INTERVAL = 0.06f;
 
         [SerializeField] private string startingMessage = default;
 
