4c711f35964242cf9abb76d4be456d907fe5a3f9
bjdr8 <baojianrispens@gmail.com>
2025-11-03 13:08:03 +0100

    arduino sends signals

--- Immerse/Assets/Bo/ArduinoCom.cs
+++ Immerse/Assets/Bo/ArduinoCom.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
 using System.Collections;
 using System.IO.Ports;
+using Immerse;
 
 public class ArduinoCom : MonoBehaviour
 {
     SerialPort sp = new SerialPort("COM5", 9600);
+    [SerializeField] Scanner scanner;
+    string savedLine;
 
     void Start()
     {
-        sp.Open();
+        sp.DtrEnable = true;
         sp.ReadTimeout = 100;
+
+        sp.Open();
         if (sp.IsOpen)
         {
             print("connected");
@@ -20,10 +25,36 @@ public class ArduinoCom : MonoBehaviour
     {
         try
         {
-            Debug.Log(sp.BytesToRead);
+            //Debug.Log(sp.BytesToRead);
+            string line = sp.ReadLine();
+            if (savedLine != line)
+            {
+                ChangeRoom(line);
+                savedLine = line;
+            }
         }
         catch (System.Exception)
         {
+        }
+    }
+
+    private void ChangeRoom(string line)
+    {
+        switch (line) {
+            case "R1":
+                scanner.Jeremy();
+                Debug.Log("Arduino: " + line);
+                break;
+            case "R2":
+                scanner.Leonardo();
+                Debug.Log("Arduino: " + line);
+                break;
+            case "R3":
+                scanner.Marc();
+                break;
+            case "R4":
+                scanner.Vivienne();
+                break;
         }
     }
 
